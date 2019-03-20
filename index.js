09337d100a2da0a75f8487765ce81736428a5fb9
RafaDraw <www.rafadraw@gmail.com>
2019-03-20 16:47:59 -0300

    complete text robot

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const robots = {
     text: require('./robots/text')
 };
 
-function start() {
+async function start() {
     const content = {};
 
     robots.userInput(content);
-    robots.text(content);
+    await robots.text(content);
 
     console.log(content);
 }
--- robots/text.js
+++ robots/text.js
@@ -1,17 +1,54 @@
 const algorithmia = require('algorithmia');
+const algorithmiaApiKey = require('../credentials/algorithmia').apikey;
+const sentenceBoundaryDetection = require('sbd');
 
-function robot(contnet) {
-    fetchContentFromWikipedia(contnet);
-    // sanitizeContent(contnet);
-    // breakContentIntoSenlinces(contnet);
+async function robot(content) {
+    await fetchContentFromWikipedia(content);
+    sanitizeContent(content);
+    breakContentIntoSenlinces(content);
 
-    function fetchContentFromWikipedia(content) {
-        const algorithmiaAuthenticated = algorithmia('API KEY TEMPORARIA');
+    async function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
-        const wikipediaResponse =  wikipediaAlgorithm.pipe(content.searchTerm);
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm);
         const wikipediaContent = wikipediaResponse.get();
 
-        console.log(wikipediaContent);
+        content.sourceContentOriginal = wikipediaContent.content;
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal);
+        const withoutDatesInParentheses = renoveDatesInParentheses(withoutBlankLinesAndMarkdown);
+
+        content.sourceContentSanitized = withoutDatesInParentheses;
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n');
+            const whithoutBlankLinesAndMarkdown = allLines.filter( line => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) return false;
+                return true
+            });
+
+            return whithoutBlankLinesAndMarkdown.join(' ')
+        }
+
+        function renoveDatesInParentheses(text) {
+            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+        }
+    }
+
+    function breakContentIntoSenlinces(content) {
+        content.sentences = [];
+
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+
+        sentences.forEach(sentence => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                imagens: []
+            })
+        });
     }
 }
 
