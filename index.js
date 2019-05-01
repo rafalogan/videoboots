c5926adb7fc7b979277a1977eadd6901b19fbffe
RafaDraw <www.rafadraw@gmail.com>
2019-05-01 15:05:23 -0300

    refector video robot

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const robots = {
     userInput: require('./robots/userInput'),
     text: require('./robots/text'),
     state: require('./robots/state'),
-    image: require('./robots/image')
+    image: require('./robots/image'),
+    video: require('./robots/video')
 };
 
 async function start() {
 
-    robots.userInput();
-    await robots.text();
-    await robots.image();
+    // robots.userInput();
+    // await robots.text();
+    // await robots.image();
+    await robots.video();
 
-    const content = robots.state.load();
-    console.dir(content, { depth: null })
+    // const content = robots.state.load();
+    // console.dir(content, { depth: null })
 }
 
 start();
